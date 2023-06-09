245a9cea90ee40ccf95d25f1dd43e55320a61fb9
Tony.Yang <Tony.Yang@Qisda.com>
2023-06-09 14:47:42 +0800

    ++ Support reboot test ++ Add USB2GPIO board test program

--- test_usb2gpio.py
+++ test_usb2gpio.py
@@ -4,18 +4,11 @@ import os
 
 cmd_write_high = '3A 16 01'
 cmd_read_high = '3D FF'
-
 usb2gpio = serial.Serial(None, 115200, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
 usb2gpio.setPort('/dev/tty.usbserial-3140')
-
 count = 1
-
 while True:
     print("==== No.%d ====" % count)
-    print("Reboot device")
-    os.system("adb reboot")
-    time.sleep(40)
-
     usb2gpio.open()
     usb2gpio.write(bytes.fromhex(cmd_read_high))
     rsp_cmd_read_high = usb2gpio.read(103)
@@ -23,12 +16,14 @@ while True:
     rsp_cmd_write_high = usb2gpio.read(2)
     usb2gpio.close()
     print(rsp_cmd_read_high)
-    rsp_str = rsp_cmd_read_high.decode()
-    if rsp_str.find("CH1:0") != -1:
+    print(rsp_cmd_write_high)
+    rx_str = rsp_cmd_read_high.decode()
+    if rx_str.find("CH1:0") != -1:
         print("Light On")
+    elif rx_str.find("CH1:1") != -1:
+        print("Light Off")
     else:
         print("ERROR!!")
         break
-
-    time.sleep(5)
     count += 1
+    time.sleep(10)
